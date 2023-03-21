6667beb25be924bfa3e5810878aeb4b0800290b3
Alejandro Gottardi <vjanger@gmail.com>
2023-03-21 12:11:17 -0300

    "entrega final js"

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 function calcularCuota(monto, cuotas) {
-  var tasa = 0.02;
+  let tasa = "0.1";
   if (document.getElementById("moneda").value === "dolares") {
-    tasa = 0.03;
+    tasa = "0.05";
   }
-  var valorCuota = (monto * tasa * Math.pow(1 + tasa, cuotas)) / (Math.pow(1 + tasa, cuotas) - 1);
+  let valorCuota = (monto * tasa * Math.pow(1 + tasa, cuotas)) / (Math.pow(1 + tasa, cuotas) - 1);
   return valorCuota.toFixed(2);
 }
 
-var ayudaCheckbox = document.getElementById("ayuda");
-var monedaSelect = document.getElementById("moneda");
+let ayudaCheckbox = document.getElementById("ayuda");
+let monedaSelect = document.getElementById("moneda");
 
 ayudaCheckbox.addEventListener("click", function() {
   if (this.checked) {
@@ -19,35 +19,29 @@ ayudaCheckbox.addEventListener("click", function() {
 });
 
 function verificarCreditoOtorgado() {
-  // Obtener el array guardado en localStorage
-  var creditos = JSON.parse(localStorage.getItem("creditos")) || [];
-
-  // Obtener los datos ingresados por el usuario
-  var nombre = document.getElementById("nombre").value;
-  var dni = document.getElementById("dni").value;
-
-  // Comprobar si el cliente ya tiene un crédito otorgado
-  for (var i = 0; i < creditos.length; i++) {
+  let creditos = JSON.parse(localStorage.getItem("creditos")) || [];
+  console.log(creditos)
+  let nombre = document.getElementById("nombre").value;
+  let dni = document.getElementById("dni").value;
+  for (let i = 0; i < creditos.length; i++) {
     if (creditos[i].nombre === nombre && creditos[i].dni === dni) {
-      var montoPrestado = creditos[i].monto;
-      var plazoPrestado = creditos[i].cuotas;
-      var valorCuota = calcularCuota(montoPrestado, plazoPrestado);
-      var cuotasRestantes = plazoPrestado - creditos[i].cuotasPagadas;
-      var mensaje = "Usted ya tiene un crédito otorgado. Le quedan " + cuotasRestantes + " cuotas de $" + valorCuota + " cada una.";
-      var resultadoDiv = document.getElementById("resultado");
+      let montoPrestado = creditos[i].monto;
+      let plazoPrestado = creditos[i].cuotas;
+      let valorCuota = calcularCuota(montoPrestado, plazoPrestado);
+      let cuotasRestantes = plazoPrestado - creditos[i].cuotasPagadas;
+      let mensaje =  "Usted ya tiene un crédito otorgado. Le quedan " + cuotasRestantes + " cuotas de $" + valorCuota + " cada una.";
+      let resultadoDiv = document.getElementById("resultado");
       resultadoDiv.innerHTML = mensaje;
       return true;
     }
   }
-
-  // Si no se encuentra un crédito otorgado, guardar los datos en el array
-  var monto = document.getElementById("monto").value;
-  var cuotas = document.getElementById("cuotas").value;
-  var moneda = document.getElementById("moneda").value;
-  var ayuda = document.getElementById("ayuda").checked;
-  var cuota = calcularCuota(monto, cuotas);
-  var fecha = new Date();
-  var nuevoCredito = {
+  let monto = document.getElementById("monto").value;
+  let cuotas = document.getElementById("cuotas").value;
+  let moneda = document.getElementById("moneda").value;
+  let ayuda = document.getElementById("ayuda").checked;
+  let cuota = calcularCuota(monto, cuotas);
+  let fecha = new Date();
+  let nuevoCredito = {
     "nombre": nombre,
     "dni": dni,
     "monto": monto,
@@ -60,9 +54,8 @@ function verificarCreditoOtorgado() {
   };
   creditos.push(nuevoCredito);
   localStorage.setItem("creditos", JSON.stringify(creditos));
-
-  // Mostrar el resultado del crédito en el DOM
-  var mensaje = "Su crédito ha sido otorgado por un monto de $" + monto + " a un plazo de " + cuotas + " cuotas de $" + cuota + " cada una.";
-  var resultadoDiv = document.getElementById("resultado");
+  console.log(nuevoCredito)
+  let mensaje = "Su crédito ha sido otorgado por un monto de $" + monto + " a un plazo de " + cuotas + " cuotas de $" + cuota + " cada una.";
+  let resultadoDiv = document.getElementById("resultado");
   resultadoDiv.innerHTML = mensaje;
 }
